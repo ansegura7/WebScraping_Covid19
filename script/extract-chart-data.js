923f9da60f1be22113166cdfdc995a2f7a025341
ansegura7 <seguraandres7@gmail.com>
2020-08-28 17:59:25 -0500

    Update extract-chart-data.js
    Updated code: more useful.

--- script/extract-chart-data.js
+++ script/extract-chart-data.js
@@ -1,3 +1,4 @@
+let result = 'date,total_cases,total_deaths\n';
 let chart_list = ['coronavirus-cases-linear', 'coronavirus-deaths-linear', 'graph-active-cases-total'];
 let chart = $('#' + chart_list[0]).highcharts();
 let chart_data1 = chart ? {'x_data': chart.xAxis[0].categories, 'y_data': chart.series[0].yData} : {};
@@ -6,11 +7,14 @@ let chart_data2 = chart ? {'x_data': chart.xAxis[0].categories, 'y_data': chart.
 
 if (chart_data1.x_data.length == chart_data2.x_data.length) {
 	let n = chart_data1.x_data.length;
-	console.log('date,total_cases,total_deaths')
+	
 	for (i=0; i<n; i++) {
 		tokens = chart_data1.x_data[i].split(' ');
 		curr_month = ("JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(tokens[0]) / 3 + 1);
 		curr_day = tokens[1];
-		console.log(curr_month + '/'  + curr_day + '/2020,' + chart_data1.y_data[i] + ',' + chart_data2.y_data[i]);
+		cure_date = curr_month + '/'  + curr_day + '/2020';
+		
+		result += cure_date + ',' + chart_data1.y_data[i] + ',' + chart_data2.y_data[i] + '\n';
 	}
 }
+console.log(result);
