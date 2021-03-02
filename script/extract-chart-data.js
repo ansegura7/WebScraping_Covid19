5dcd824cfdfba62a2abfe40914b4120e4eefb3b2
Andres Segura <seguraandres7@gmail.com>
2021-03-02 12:14:47 -0500

    Update extract-chart-data.js
    Added new_cases and new_deaths variables.

--- script/extract-chart-data.js
+++ script/extract-chart-data.js
@@ -1,5 +1,5 @@
 function extractData() {
-	let result = 'date,total_cases,total_deaths\n';
+	let result = 'date,total_cases,total_deaths,new_cases,new_deaths\n';
 	let chart_list = ['coronavirus-cases-linear', 'coronavirus-deaths-linear', 'graph-active-cases-total'];
 	let chart = $('#' + chart_list[0]).highcharts();
 	let chart_data1 = chart ? { 'x_data': chart.xAxis[0].categories, 'y_data': chart.series[0].yData } : {};
@@ -13,9 +13,13 @@ function extractData() {
 			tokens = chart_data1.x_data[i].split(' ');
 			curr_month = ("JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(tokens[0]) / 3 + 1);
 			curr_day = tokens[1];
-			cure_date = curr_month + '/' + curr_day + '/2020';
+			curr_date = curr_month + '/' + curr_day + '/2020';
+			new_cases = (i > 0 ? chart_data1.y_data[i] - curr_cases : 0);
+			new_deaths = (i > 0 ? chart_data2.y_data[i] - curr_deaths : 0);
+			curr_cases = chart_data1.y_data[i];
+			curr_deaths = chart_data2.y_data[i];
 
-			result += cure_date + ',' + chart_data1.y_data[i] + ',' + chart_data2.y_data[i] + '\n';
+			result += curr_date + ',' + curr_cases + ',' + curr_deaths + ',' + new_cases + ',' + new_deaths + '\n';
 		}
 	}
 	return result;
